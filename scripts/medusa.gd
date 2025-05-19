22750f128f990559b43ae1ab0246e881116ad44c
Acherow <victorhugodias1@hotmail.com>
2025-05-19 20:12:52 -0300

    sgnosfgsfgs

--- scripts/medusa.gd
+++ scripts/medusa.gd
@@ -13,16 +13,18 @@ func _ready():
 	raycast.add_exception(self)
 	noise.modulate.a = 0
 
+var died : bool
 func _physics_process(delta):
 	return
 	raycast.target_position = raycast.to_local(player.global_position) + Vector3.UP
-	if(seen()):
+	if(seen() && !died):
 		noise.modulate.a = .5-(fps/40)
 		#noise.texture.noise.seed = randi_range(0,4)
 		fps = clamp(fps,2,40)
 		fps -= delta * 20
 		if(fps <= 2):
 			player.ragdoll(100)
+			died = true
 	else:
 		noise.modulate.a = lerp(noise.modulate.a, 0.0, delta*10)
 		fps = 60
