5cb3b1e641c9eb3749d270be1a658f08cb3e4645
Acherow <victorhugodias1@hotmail.com>
2025-05-20 00:42:15 -0300

    hgddghdg

--- scripts/medusa.gd
+++ scripts/medusa.gd
@@ -1,4 +1,4 @@
-extends Node3D
+extends RigidBody3D
 
 @onready var raycast = $Skeleton3D/BoneAttachment3D/head/RayCast3D
 @onready var noise = $CanvasLayer/noise
@@ -6,6 +6,9 @@ extends Node3D
 var player : Node3D
 
 var fps : float = 60
+
+@export var patrol : Array[Vector3]
+var curpoint : int = 0
 
 func _ready():
 	player = get_tree().get_first_node_in_group("player")
@@ -15,6 +18,14 @@ func _ready():
 
 var died : bool
 func _physics_process(delta):
+	if(global_position.distance_to(patrol[curpoint]) < .2):
+		curpoint = (curpoint+1) % patrol.size()
+	
+	apply_central_force((global_position.direction_to(patrol[curpoint])).normalized() * 600)
+	#global_basis = lerp(global_basis,global_basis.looking_at((patrol[curpoint]-global_position)/2),delta * 10)
+	#move_and_slide()
+	var targetbasis = global_basis.looking_at(linear_velocity,Vector3.UP,false)
+	apply_torque(Library.calc_angular_velocity(global_basis,targetbasis)*10)
 	return
 	raycast.target_position = raycast.to_local(player.global_position) + Vector3.UP
 	if(seen() && !died):
